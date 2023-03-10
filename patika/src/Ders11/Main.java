5c58065af6cdd228d3710a6da693c0e644e0a1a1
hasanCets <126797484+hasanCets@users.noreply.github.com>
2023-03-10 12:22:14 +0300

    www.patika.dev
    karşılaştırma operatörleri

---
+++ patika/src/Ders11/Main.java
@@ -0,0 +1,21 @@
+package Ders11;
+
+public class Main {
+    public static void main(String[] args){
+
+        int a = 10, b = 20, c;
+
+        /*
+        boolean kosul1 = (a < b);
+        boolean kosul2 = (a > c);
+        boolean sonuc = kosul1 && kosul2;
+
+        System.out.println(sonuc);
+        */
+        c = (a == b) ? 2 : 3;
+        System.out.println(c);
+
+
+
+    }
+}
