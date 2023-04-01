09661d5ec2bf89bc070d9570c862d2566d71ea94
hasanCets <126797484+hasanCets@users.noreply.github.com>
2023-04-01 16:36:35 +0300

    Delete patika/src/Ders7 directory

--- patika/src/Ders7/Main.java
+++
@@ -1,9 +0,0 @@
-package Ders7;
-
-//String Veri Tipi (charların birleşimi)
-public class Main {
-    public static void main(String[] args){
-       String vStr = "Java101 Patikası";
-       System.out.println(vStr);
-    }
-}
