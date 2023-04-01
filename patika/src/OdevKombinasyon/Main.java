38cadb5a98f55c35acae94ae523c6f55f2a60686
hasanCets <126797484+hasanCets@users.noreply.github.com>
2023-04-01 16:39:09 +0300

    Kombinasyon hesabı yapan program.

---
+++ patika/src/OdevKombinasyon/Main.java
@@ -0,0 +1,40 @@
+package Donguler.OdevKombinasyon;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        int n,r;
+        int nFact = 1, rFact = 1, n_rFact =1;
+        double combination;
+
+        Scanner inp = new Scanner (System.in);
+
+        System.out.print("1. Değeri giriniz :");
+        n = inp.nextInt();
+
+        System.out.print("2. Değeri giriniz :");
+        r = inp.nextInt();
+
+        for (int i = 1; i <= n; i++){
+            nFact *= i;
+        }
+
+        for (int j = 1; j <= r; j++){
+            rFact *= j;
+        }
+        for (int k = 1; k <= (n-r); k++){
+            n_rFact *=k;
+        }
+
+        combination = nFact / (rFact*n_rFact);
+
+        System.out.println("Sonuç : " + combination);
+
+
+
+
+
+
+
+    }
+}
+
