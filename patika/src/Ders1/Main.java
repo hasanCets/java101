f70a5125a4578852d20eefbd2a3916a7f56b8893
hasanCets <126797484+hasanCets@users.noreply.github.com>
2023-04-01 16:35:42 +0300

    Delete patika/src/Ders1 directory

--- patika/src/Ders1/Main.java
+++
@@ -1,8 +0,0 @@
-package Ders1;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Java101 Patikası");
-        System.out.print("Başlıyoruz");
-    }
-}
